7d0d26c0c93a86251618aed0ff5968c3429bf5c3
timwlee <timothyl@adobe.com>
2025-06-17 12:55:08 -0700

    WIP

--- blocks/highlight/highlight.js
+++ blocks/highlight/highlight.js
@@ -2,5 +2,9 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  console.log("highlight block: ", block);
+  // Optimize Image
+  const backgroundImage = block.querySelector('picture > img');
+  const optimizedPic = createOptimizedPicture(backgroundImage.src, backgroundImage.alt, false, [{ width: '1600' }]);
+  moveInstrumentation(backgroundImage, optimizedPic.querySelector('img'));
+  backgroundImage.closest('picture').replaceWith(optimizedPic);
 }
